f2b382412b11372321a8cea66d15670d1bbf7632
Top_Teera <teerawutkt3@gmail.com>
2019-08-25 12:01:25 +0700

    web service

---
+++ src/main/java/com/proch/project/ws/rest/RestService.java
@@ -0,0 +1,18 @@
+package com.proch.project.ws.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.baeldung.springsoap.gen.Country;
+import com.proch.project.ws.sope.CountryRepository;
+
+@Service
+public class RestService {
+
+	@Autowired
+	private CountryRepository countryRepository;
+	
+	public Country getCountry(Request request) {
+		return countryRepository.findCountry(request.getName());
+	}
+}
