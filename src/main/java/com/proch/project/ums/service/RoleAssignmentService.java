1fdc2003c7966cae6ae070b7620fd46ea57056dc
Top_Teera <teerawutkt3@gmail.com>
2019-11-25 18:18:45 +0700

    crud table many to many

--- src/main/java/com/proch/project/ums/service/RoleAssignmentService.java
+++ src/main/java/com/proch/project/ums/service/RoleAssignmentService.java
@@ -1,19 +1,16 @@
 package com.proch.project.ums.service;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.proch.project.ums.entity.Role;
 import com.proch.project.ums.entity.User;
 import com.proch.project.ums.repository.RoleRepository;
 import com.proch.project.ums.repository.UserRepository;
 import com.proch.project.ums.vo.RoleAssignmentVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.EntityManager;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class RoleAssignmentService {
@@ -26,25 +23,24 @@ public class RoleAssignmentService {
 
 	@Transactional
 	public User save(RoleAssignmentVo vo) {
-
 		User user = userRepository.findById(vo.getUserId()).get();
-		List<Role> hasRole = new ArrayList<Role>();
-		List<User> hasUser = new ArrayList<User>();
+
+		List<Role> hasRole = new ArrayList<>();
+		List<User> hasUser = new ArrayList<>();
 
 		for (Role role : user.getRoles()) {
 			role.getUsers().remove(user);
+			user.setRoles(hasRole);
 			roleRepository.save(role);
 		}
-		user.getRoles().clear();
 		user = userRepository.save(user);
 
 		user.setRoles(hasRole);
 		hasUser.add(user);
 		for (Long roleId : vo.getRoleIds()) {
 			Role role = roleRepository.findById(roleId).get();
-
 			hasRole.add(role);
-			role.setUsers(hasUser);
+			role.getUsers().add(user);
 			user.setRoles(hasRole);
 		}
 		user = userRepository.save(user);
